5d5b95f4c4dfe921eac2bfddf11a5be6e7550039
ceejjay <cheidick@gmail.com>
2025-02-05 21:40:11 -0600

    adding modified files

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
 	pygame.init()
@@ -9,15 +11,27 @@ def main():
 
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+
+	Player.containers = (updatable, drawable)
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
+
 	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+	AsteroidField()
 
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0,0,0))
-		player.draw(screen)
+		updatable.update(dt)
+		for spri in drawable:
+			spri.draw(screen)
 		pygame.display.flip()
-		dt = game_clock.tick(60)/1000
+		dt = game_clock.tick(60) / 1000
+
 if __name__ == "__main__":
 	main()
